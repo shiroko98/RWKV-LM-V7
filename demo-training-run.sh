465ac90a3491c57b56e84bee7697a4034f8b27ca
Ce Wang <halowang1991@gmail.com>
2025-06-15 07:52:06 +0800

    [Refactor] Simplify training script parameters (#4)
    * [Refactor] Simplify training script parameters
    
    * Extracted common parameters into variables for better readability and maintainability in demo-training-prepare.sh and demo-training-run.sh.
    * Updated the command structure for clarity and consistency across both scripts.

--- demo-training-run.sh
+++ demo-training-run.sh
@@ -41,10 +41,43 @@ GPU_PER_NODE=1 # number of GPUs per node
 #
 DS_BUCKET_MB=2 # set to 2 for consumer GPUs, set to 200 for A100 / H100 (affects speed & vram usage)
 #
-python train.py --load_model "0" --wandb "Test" --proj_dir $PROJ_DIR --my_testing $MODEL_TYPE \
- --ctx_len $CTX_LEN --train_stage 3 --epoch_count 999999 --epoch_begin 0 \
- --data_file "data/minipile" --my_exit_tokens 1498226207 --magic_prime 2926181 \
- --num_nodes $N_NODE --micro_bsz $M_BSZ --n_layer $N_LAYER --n_embd $N_EMBD \
- --lr_init $LR_INIT --lr_final $LR_FINAL --warmup_steps 10 --beta1 0.9 --beta2 0.99 --adam_eps 1e-18 --data_type "binidx" --vocab_size 65536 \
- --weight_decay 0.001 --epoch_save $EPOCH_SAVE --head_size 64 \
- --accelerator gpu --devices $GPU_PER_NODE --precision bf16 --strategy deepspeed_stage_2 --grad_cp $GRAD_CP --enable_progress_bar True --ds_bucket_mb $DS_BUCKET_MB
+#
+MY_EXIT_TOKENS="1498226207"
+MAGIC_PRIME="2926181"
+DATA_FILE="data/minipile"
+DATA_TYPE="binidx"
+#
+python train.py \
+ --wandb "" \
+ --accelerator gpu \
+ --adam_eps 1e-18 \
+ --beta1 0.9 \
+ --beta2 0.99 \
+ --ctx_len $CTX_LEN \
+ --data_file $DATA_FILE \
+ --data_type $DATA_TYPE \
+ --devices $GPU_PER_NODE \
+ --ds_bucket_mb $DS_BUCKET_MB \
+ --enable_progress_bar True \
+ --epoch_begin 0 \
+ --epoch_count 999999 \
+ --epoch_save $EPOCH_SAVE \
+ --grad_cp $GRAD_CP \
+ --head_size 64 \
+ --load_model "0" \
+ --lr_final $LR_FINAL \
+ --lr_init $LR_INIT \
+ --magic_prime $MAGIC_PRIME \
+ --micro_bsz $M_BSZ \
+ --my_exit_tokens $MY_EXIT_TOKENS \
+ --my_testing $MODEL_TYPE \
+ --n_embd $N_EMBD \
+ --n_layer $N_LAYER \
+ --num_nodes $N_NODE \
+ --precision bf16 \
+ --proj_dir $PROJ_DIR \
+ --strategy deepspeed_stage_2 \
+ --train_stage 3 \
+ --vocab_size 65536 \
+ --warmup_steps 10 \
+ --weight_decay 0.001 \
